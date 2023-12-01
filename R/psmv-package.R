8793ed077717526bdb9a52a60909062622c74a86
Ranke Johannes <johannes.ranke@agroscope.admin.ch>
2023-12-01 09:54:10 +0100

    Add R/psmv-package.R with @importFrom statements

---
+++ R/psmv-package.R
@@ -0,0 +1,17 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr arrange
+#' @importFrom dplyr if_else
+#' @importFrom dplyr mutate 
+#' @importFrom dplyr group_by
+#' @importFrom dplyr mutate
+#' @importFrom dplyr select
+#' @importFrom dplyr ungroup 
+#' @importFrom xml2 xml_attr 
+#' @importFrom xml2 xml_attrs
+#' @importFrom xml2 xml_children
+#' @importFrom xml2 xml_find_all
+## usethis namespace: end
+NULL
