d033436ae722c75173d96615bdfa7248a5c9e5bb
Ranke Johannes <johannes.ranke@agroscope.admin.ch>
2024-03-04 09:03:51 +0100

    Skip l/ha if no content in g/L available per default
    This default is consistent with the original indicator project

--- R/use-rates.R
+++ R/use-rates.R
@@ -12,6 +12,10 @@
 #' in a [psmv_dm] object, as well as the columns 'percent' and 'g_per_L'
 #' from the 'ingredients' table in a [psmv_dm] object.
 #' @param aggregation How to represent a range if present, e.g. "mean" or "max"
+#' @param skip_l_per_ha_without_g_per_l Per default, uses where the use rate
+#' has units of l/ha are skipped, if there is not product concentration
+#' in g/L. This was also done in the 2023 indicator project.
+#'
 #' @return A tibble containing one additional column 'rate_g_per_ha'
 #' @export
 #' @examples
@@ -19,17 +23,17 @@
 #' library(dplyr)
 #' library(dm)
 #' psmv <- psmv_list[["2022"]]
-#'   
+#'
 #' product_uses_with_ingredients <- psmv |>
-#'   dm_filter(substances = 
+#'   dm_filter(substances =
 #'     (substance_de %in% c("Halauxifen-methyl", "Kupfer (als Kalkpr\u00E4parat)"))) |>
 #'   dm_flatten_to_tbl(uses, products) |>
 #'   left_join(psmv$ingredients, by = join_by(wNbr),
 #'     relationship = "many-to-many") |>
 #'   left_join(psmv$substances, by = join_by(pk)) |>
-#'   select(wNbr, name, use_nr, 
+#'   select(wNbr, name, use_nr,
 #'     min_dosage, max_dosage, min_rate, max_rate, units_de,
-#'     application_area_de, 
+#'     application_area_de,
 #'     substance_de, percent, g_per_L)
 #'
 #' application_rate_g_per_ha(product_uses_with_ingredients) |>
@@ -40,16 +44,17 @@
 #'     units_de, rate = rate_g_per_ha) |>
 #'   print(n = Inf)
 application_rate_g_per_ha <- function(product_uses,
-  aggregation = c("mean", "max", "min"))
+  aggregation = c("mean", "max", "min"),
+  skip_l_per_ha_without_g_per_L = TRUE)
 {
   aggregation = match.arg(aggregation)
-  product_rates_dosage <- product_uses |>
+  rates_dosages <- product_uses |> # Rates are called "Expenditures" in the XML
     mutate( # First we set zeros to NA, as this is what they are
       min_rate = na_if(min_rate, 0),
       max_rate = na_if(max_rate, 0),
       min_dosage = na_if(min_dosage, 0),
       max_dosage = na_if(max_dosage, 0)) |>
-    mutate( # Then we make ranges, possbily with min equals max
+    mutate( # Then we make ranges, possibly with min equals max
       rate_min = min_rate, # 'expenditures' are use rates (Aufwandmengen)
       rate_max = if_else(is.na(max_rate), # if zero, either no range or only conc
         min_rate, max_rate),
@@ -58,7 +63,7 @@ application_rate_g_per_ha <- function(product_uses,
         min_dosage, max_dosage)
       ) |>
     mutate(
-      product_rate = case_when(
+      rate = case_when(
         aggregation == "mean" ~ (rate_min + rate_max)/2,
         aggregation == "max" ~ rate_max,
         aggregation == "min" ~ rate_min
@@ -70,22 +75,25 @@ application_rate_g_per_ha <- function(product_uses,
       )
     )
 
-  active_rates <- product_rates_dosage |>
+  active_rates <- rates_dosages |>
     mutate(percent = as.numeric(percent), g_per_L = as.numeric(g_per_L)) |>
-    mutate(g_per_L = if_else(is.na(g_per_L),
-      percent * 10, g_per_L)) |> # Assume density 1 kg/L if necessary
     mutate(ref_volume = case_when(
       application_area_de %in% c("Feldbau", "Gem\u00FCsebau", "Beerenbau", "Zierpflanzen") ~ 1000,
       application_area_de %in% c("Weinbau", "Obstbau") ~ 1600,
       .default = NA)) |>
     mutate(rate_g_per_ha = case_when(
-      units_de == "l/ha" ~ product_rate * g_per_L,
-      units_de == "kg/ha" ~ product_rate * (percent * 10), # percent w/w means 10 g/kg
-      units_de == "g/ha" ~ product_rate * (percent / 100), # percent w/w means 0.01 g/g
-      units_de == "ml/m\u00B2" ~ (product_rate/1000) * (g_per_L) * 10000,
-      units_de == "ml/10m\u00B2" ~ (product_rate/1000) * (g_per_L) * 100000,
-      units_de == "ml/ha" ~ (product_rate/1000) * (g_per_L),
-      units_de == "ml/a" ~ (product_rate/1000) * (g_per_L) * 100,
+      units_de == "l/ha" ~ if_else(is.na(g_per_L), # For l/ha, if g_per_L is not defined,
+        if (skip_l_per_ha_without_g_per_L) NA
+        else rate * dosage * (percent/100), # Rate is assumed to be water,
+          # dosage is assumed to be g product per L. This is correct for
+          # Rhodofix 2009 (Grünbuch) and 2012 (XML)
+        rate * g_per_L),
+      units_de == "kg/ha" ~ rate * (percent * 10), # percent w/w means 10 g/kg
+      units_de == "g/ha" ~ rate * (percent / 100), # percent w/w means 0.01 g/g
+      units_de == "ml/m\u00B2" ~ (rate/1000) * (g_per_L) * 10000,
+      units_de == "ml/10m\u00B2" ~ (rate/1000) * (g_per_L) * 100000,
+      units_de == "ml/ha" ~ (rate/1000) * (g_per_L),
+      units_de == "ml/a" ~ (rate/1000) * (g_per_L) * 100,
       is.na(units_de) ~ ref_volume * dosage/100 * g_per_L,
       .default = NA))
   ret <- bind_cols(product_uses, active_rates["rate_g_per_ha"])
