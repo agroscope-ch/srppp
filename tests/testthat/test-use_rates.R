1473eb903b9c584c0d0c15bebbe46804dbc4f8c0
Ranke Johannes <johannes.ranke@agroscope.admin.ch>
2024-10-25 16:02:25 +0200

    Test use rate in g/L for liquids with only dosage

--- tests/testthat/test-use_rates.R
+++ tests/testthat/test-use_rates.R
@@ -34,4 +34,33 @@ test_that("Use rates are correctly converted to g/ha", {
     application_rate_g_per_ha(uses_fosetyl)$rate_g_per_ha,
     c(4000, 1600, 3840, 6000, 1600, 2400, 2000)) # Reference generated in the PAIV indicator project
 
+  # A use of Difcor 250 EC on strawberries
+  # sr <- srppphist::srppp_list[["2024"]]
+  # sr$products |>
+  #   filter(name == "Misto 12") |>
+  #   left_join(sr$uses, by = "pNbr") |>
+  #   left_join(sr$cultures, by = c("pNbr", "use_nr")) |>
+  #   filter(culture_de %in% c("Kernobst")) |>
+  #   left_join(sr$ingredients, by = "pNbr", relationship = "many-to-many") |>
+  #   filter(type == "ACTIVE_INGREDIENT") |>
+  #   select(pNbr, wNbr, use_nr, application_area_de, culture_de,
+  #     min_dosage, max_dosage, min_rate, max_rate, units_de, percent, g_per_L)
+
+  uses_Misto_12 <- tibble::tibble(
+    pNbr = 1865,
+    use_nr = c(2, 4, 7),
+    application_area_de = "Obstbau",
+    culture_de = "Kernobst",
+    min_dosage = c(2, 1, 3.5),
+    max_dosage = NA,
+    min_rate = NA, # 32, 16, 56, to be reproduced as intermediate result
+    max_rate = NA,
+    units_de = NA,
+    percent = 99.1,
+    g_per_L = 830)
+
+  expect_equal(
+    application_rate_g_per_ha(uses_Misto_12)$rate_g_per_ha,
+    c(32, 16, 56) * 830)
+
 })
