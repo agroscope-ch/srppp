ba4a560f9335d5f407c1106a00785c7c53f1274c
Ranke Johannes <johannes.ranke@agroscope.admin.ch>
2025-01-21 15:32:33 +0100

    Add tests for resolve_cultures()

--- tests/testthat/test-srppp_xml.R
+++ tests/testthat/test-srppp_xml.R
@@ -1,22 +1,9 @@
-test_that("We get a dm object with relational integrity", {
-
-  # Read in the current SRPPP from the XML URL
-  # Interception of the system message by sink() was taken from https://stackoverflow.com/a/66139071
-  nullcon <- file(nullfile(), open = "wb")
-  sink(nullcon, type = "message")
-  srppp_cur <- srppp_dm()
-  sink(type = "message")
-  close(nullcon)
+test_that("We get dm objects with relational integrity", {
 
   expect_message(
     print(dm::dm_examine_constraints(srppp_cur, .progress = FALSE)),
     "All constraints satisfied")
 
-  # Read in the test dataset included in the package
-  test_data <- system.file("testdata/Daten_Pflanzenschutzmittelverzeichnis_2024-12-16.zip",
-    package = "srppp")
-  srppp_test_xml <- srppp_xml_get_from_path(test_data, from = "2024-12-16")
-  srppp_test <- srppp_dm(srppp_test_xml)
   expect_message(
     print(dm::dm_examine_constraints(srppp_test, .progress = FALSE)),
     "All constraints satisfied")
