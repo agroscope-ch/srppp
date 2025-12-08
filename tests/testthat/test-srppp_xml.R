c0dcaf8b0e381ad3cc4ed1b414dd11f93e7e47df
Ranke Johannes <johannes.ranke@agroscope.admin.ch>
2025-12-08 08:12:18 +0100

    Adapt test to new test data
    Three reference products for parallel imports are missing.

--- tests/testthat/test-srppp_xml.R
+++ tests/testthat/test-srppp_xml.R
@@ -8,8 +8,19 @@ test_that("We get dm objects with relational integrity", {
     print(dm::dm_examine_constraints(srppp_test_1, .progress = FALSE)),
     "All constraints satisfied")
 
-  expect_message(
-    print(dm::dm_examine_constraints(srppp_test_2, .progress = FALSE)),
-    "All constraints satisfied")
+  constraints_test_2 <- dm::dm_examine_constraints(srppp_test_2, .progress = FALSE) |> 
+    as_tibble()
+ 
+  # Count the number of keys that failed a constraint check
+  n_failed <- constraints_test_2 |> 
+    filter(!is_key) |> 
+    nrow()
+  expect_equal(n_failed, 1)
 
+  # We know there is a problem with the current test data, as three reference 
+  # products parallel imports are missing
+  expect_equal(
+    constraints_test_2[[1, "problem"]],
+    "values of `parallel_imports$pNbr` not in `pNbrs$pNbr`: 7738 (5), 8332 (1), 9033 (1)")
+    
 })
